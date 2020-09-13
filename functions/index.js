265597ab6be36670d23669da4521925616c14ee0
tamayan <tamayan.db18+dev@gmail.com>
2020-09-13 15:02:11 +0900

    Firebase認証に変更

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,49 @@
 const functions = require('firebase-functions');
-const express = require('express')
-const basicAuth = require('express-basic-auth')
+const admin = require('firebase-admin');
+const express = require('express');
 
-const app = express()
+// const cookieParser = require('cookie-parser')();
+// const cors = require('cors')({
+//     origin: true
+// });_
 
-const pass = `${functions.config().basic.pass}`
+const app = express();
 
-app.use(basicAuth({
-    users: {
-        'admin': pass
-    },
-    challenge: true
-}));
+admin.initializeApp();
+
+const validateFirebaseIdToken = async (req, res, next) => {
+    if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+        !(req.cookies && req.cookies.__session)) {
+        res.status(403).send('Unauthorized');
+        return;
+    }
+
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else if (req.cookies) {
+        idToken = req.cookies.__session;
+    } else {
+        res.status(403).send('Unauthorized');
+        return;
+    }
+
+    try {
+        const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+        req.user = decodedIdToken;
+
+        next();
+        return;
+    } catch (error) {
+        res.status(403).send('Unauthorized');
+        return;
+    }
+};
+
+// app.use(cors);
+// app.use(cookieParser);
+
+app.use(validateFirebaseIdToken);
 
 app.get("/users", (req, res) => {
     res.json(require('./data/users.json'))
@@ -21,8 +53,4 @@ app.get("/videos", (req, res) => {
     res.json(require('./data/videos.json'))
 });
 
-const api = functions.https.onRequest(app);
-
-module.exports = {
-    api
-};
+exports.app = functions.https.onRequest(app);
